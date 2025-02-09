c61c5f2f8e72bd10c1ae92bd6b0bfefb700502a0
George Gencarelli <george.gencarelli@gmail.com>
2025-02-08 20:44:52 -0500

    whatever

--- bskyfollows.py
+++ bskyfollows.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 #title           :bskyfollows.py
-#description     :This will follow anyone who's following you that you haven't
-#                 followed back. It will also unfollow anyone who you've
-#                 followed but who hasn't followed you back.
+#description     : Automated follows management
 #author          :tellynumner
-#date            :01/31/25
-#version         :0.3
+#date            :02/08/25
+#version         :0.5
 #usage           :./bskyfollows.py
 #notes           :
 #python_version  :3.12.3
@@ -20,9 +18,9 @@ client = Client()
 client.login(user, pasw)
 
 def logger(level, message):
-    '''Logs to a file and prints to the screen'''
+    '''Logs a message to a file and prints it to the screen.'''
     now = datetime.datetime.now()
-    ts = '[' + now.strftime("%a, %d %b %Y %H:%M:%S") + ']'
+    ts = '[' + now.strftime("%d %b %Y %H:%M:%S") + ']'
     copy = ts + ' ' + level + ' ' + message
     with open('bskyfollows.log', 'a') as lf:
         lf.write(copy + '\n')
@@ -30,7 +28,7 @@ def logger(level, message):
     return None
 
 def get_followers():
-    '''Collects profile data on the list of accounts following us'''
+    '''Collects profile data on the list of accounts following us.'''
     followers = []
     response = client.get_followers(user, None, 100)
     followers.extend(response.followers)
@@ -39,8 +37,8 @@ def get_followers():
         followers.extend(response.followers)
     return followers
 
-def get_following():
-    '''Collects profile data on the list of accounts we follow'''
+def get_follows():
+    '''Collects profile data on the list of accounts we follow.'''
     follows = []
     response = client.get_follows(user, None, 100)
     follows.extend(response.follows)
@@ -61,32 +59,81 @@ def follow_back(follower_list):
     logger('INFO', "I followed back " + str(count) + " accounts!")
     return None
 
-def unfollow(follows_list):
-    '''Unfollows any account not following us'''
-    count = 0
+def last_post_date(user):
+    """
+    Gets the date of the last post of a user. Reports if the last post was more recent than 15
+    days ago.
+    """
+    fifteen_days_ago = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=15)
+    last_post = ''
+    try:
+        response = client.get_author_feed(user.handle)
+    except:
+        last_post = datetime.datetime.fromisoformat('2001-01-01 01:01:01.858000+00:00')
+    if last_post == '':
+        if len(response.feed) > 0:
+            try:
+                last_post = datetime.datetime.fromisoformat(response.feed[1].post.indexed_at)
+            except:
+                last_post = datetime.datetime.fromisoformat('2001-01-01 01:01:02.858000+00:00')
+        else:
+            last_post = datetime.datetime.fromisoformat('2001-01-01 01:01:03.858000+00:00')
+    if last_post > fifteen_days_ago:
+        return 'current'
+    else:
+        return 'older than 15 days'
+
+def manage_follows(follows_list):
+    '''
+    Unfollows the accounts we follow based on whether or not they follow us, their follower ratio, 
+    and the last time they posted. IF an account has not posted in 15 days, whether or not they 
+    follow us, they are deleted. IF an account does not follow us AND IF their follower ratio is 
+    not better than 1, they are deleted. 
+    '''
     for follow in follows_list:
+        flag = 'n'
+        last_post = last_post_date(follow)
+        profile = client.get_profile(follow.did)
+        followers = int(profile.followers_count)
+        follows = int(profile.follows_count)
+        if followers != 0 and follows != 0:
+            follower_ratio = followers / follows
+        else:
+            follower_ratio = .5
+        logger('INFO', 'Scrutinizing following relationship: ' + follow.handle + '...')
+        logger('INFO', 'Checking to see if ' + follow.handle + ' has posted recently...')
+        logger('INFO', 'Post history is ' + last_post + '.')
+        if last_post != 'current':
+            logger('DELETEFOLLOW', follow.handle + " hasn't posted in the last 15 or more days.")
+            flag = 'y'
+        logger('INFO', 'Checking to see if ' + follow.handle + ' follows us...')
         if follow.viewer['followed_by'] == None:
-            count += 1
-            logger('ASSHOLE', follow.handle + " hasn't followed me back.")
-            if follow.viewer['following'] is not None:
-                client.delete_follow(follow.viewer['following'])
-                logger('INFO', "Unfollowed " + follow.handle + "!")
-                time.sleep(0.5)
+            logger('INFO', follow.handle + " doesn't follow us. Let's check if they're an influencer.")
+            if follower_ratio > 1:
+                logger('INFO', follow.handle + ' is an influencer.')
+                flag = 'n'
             else:
-                logger('SHIT', follow.handle + " doesn't have a following link! Manually remove!")
-    logger('INFO', "I unfollowed " + str(count) + " accounts!")
+                logger('DELETEFOLLOW', follow.handle + " isn't an influencer.")
+                flag = 'y'
+        else:
+            logger('INFO', follow.handle + ' follows us.')
+        if flag == 'n':
+            logger('INFO', 'Preserving following relationship with ' + follow.handle + '.')
+        else:
+            client.delete_follow(follow.viewer['following'])
+            logger('FOLLOWDELETED', follow.handle + ' unfollowed.')
     return None
 
 def main():
     '''Main processing function'''
-    logger('INFO', 'Beginning follower management for ' + user)
+    logger('INFO', 'Beginning follower management for ' + user + '.')
     logger('INFO', 'Processing followers list...')
     followers = get_followers()
     follow_back(followers)
     logger('INFO', 'Processing following list...')
-    following = get_following()
-    unfollow(following)
-    logger('HOORAY', 'All done!')
+    following = get_follows()
+    manage_follows(following)
+    logger('DONE', 'Finished!')
 
 if __name__ == '__main__':
     main()
